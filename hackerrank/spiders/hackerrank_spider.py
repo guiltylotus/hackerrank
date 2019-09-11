50861ca3e70520e7f0f422cb1558398d654b0355
Lotus Guilty <minh02091997@gmail.com>
2019-09-11 19:01:50 +0700

    first_run

--- hackerrank/spiders/hackerrank_spider.py
+++ hackerrank/spiders/hackerrank_spider.py
@@ -3,10 +3,14 @@ import scrapy
 
 
 class HackerrankScraper(scrapy.Spider):
-	name = 'hackerrank'
-	start_urls = ["https://www.hackerrank.com/rest/contests/master/tracks/algorithms/challenges?offset=0&limit=500&track_login=true"]
+    name = 'hackerrank'
+    start_urls = [
+        "https://www.hackerrank.com/rest/contests/master/tracks/algorithms/challenges?offset=0&limit=50&track_login=true"]
 
-	def parse(self, response):
-		data = json.loads(response.text)
-		print('______________________')
-		print(len(data["models"]))
+    def parse(self, response):
+        datas = json.loads(response.text)["models"]
+
+        for data in datas:
+            yield {
+                'name': data["name"]
+            }
