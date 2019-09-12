0427ef2e66fa3bebf9d23ccfcaa73f9bb3f1d079
Lotus Guilty <minh02091997@gmail.com>
2019-09-12 16:24:27 +0700

    Items stored

--- hackerrank/items.py
+++ hackerrank/items.py
@@ -12,3 +12,21 @@ class HackerrankItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ProblemList(scrapy.Item):
+    name = scrapy.Field()
+    url = scrapy.Field()
+    success_ratio = scrapy.Field()
+    max_score = scrapy.Field()
+    difficulty_name = scrapy.Field()
+
+class ProblemDetail(scrapy.Item):
+    problem = scrapy.Field()
+    sample_input = scrapy.Field()
+    sample_output = scrapy.Field()
+    problem_name = scrapy.Field()
+
+class Leader(scrapy.Item):
+    username = scrapy.Field()
+    rank = scrapy.Field()
+    score = scrapy.Field()
--- hackerrank/spiders/hackerrank_spider.py
+++ hackerrank/spiders/hackerrank_spider.py
@@ -1,16 +1,17 @@
 import json
 import scrapy
-
+from hackerrank.items import ProblemList, ProblemDetail, Leader
 
 class HackerrankScraper(scrapy.Spider):
     def __init__(self):
         self.offset = 0
 
     name = 'hackerrank'
-    problems_api = "https://www.hackerrank.com/rest/contests/master/tracks/algorithms/challenges?offset={}&limit=2&track_login=true"
+    problems_api = "https://www.hackerrank.com/rest/contests/master/tracks/algorithms/challenges?offset={}&limit=50&track_login=true"
     start_urls = [problems_api.format(0)]
 
     def parse(self, response):
+        items = ProblemList()
         datas = json.loads(response.text)["models"]
 
         for data in datas:
@@ -19,18 +20,24 @@ class HackerrankScraper(scrapy.Spider):
             leader_board_url = 'https://www.hackerrank.com/challenges/' + \
                 data['slug'] + '/leaderboard?limit=100&page=1'
 
-            yield {
-                'name': data["name"],
-                'url': problem_url
-            }
-            yield scrapy.Request(url=problem_url, callback=self.parseProblems)
-            yield scrapy.Request(url=leader_board_url, callback=self.parseLeaderBoard, cb_kwargs=dict(problem_name=data["name"]))
+            items['name'] = data["name"],
+            items['url'] = problem_url,
+            items['success_ratio'] = data["success_ratio"]
+            items['max_score'] = data["max_score"]
+            items['difficulty_name'] = data["difficulty_name"]
 
-        if len(datas) > 0 and self.offset < 3:
-            self.offset = self.offset + 2
+            yield items
+
+            yield scrapy.Request(url=problem_url, callback=self.parseProblems, cb_kwargs=dict(problem_name=data["name"]))
+            # yield scrapy.Request(url=leader_board_url, callback=self.parseLeaderBoard, cb_kwargs=dict(problem_name=data["name"]))
+
+        if len(datas) > 0:
+            self.offset = self.offset + 50
             yield scrapy.Request(url=self.problems_api.format(self.offset), callback=self.parse)
 
-    def parseProblems(self, response):
+    def parseProblems(self, response, problem_name):
+        items = ProblemDetail()
+
         data = response.css(
             "div.challenge_problem_statement div.hackdown-content").css('p, ul').css('::text').extract()
 
@@ -49,21 +56,21 @@ class HackerrankScraper(scrapy.Spider):
         outputStatement = ' '.join(data)
         outputStatement = ' '.join(inputStatement.split())
 
-        yield {
-            'sample_input': inputStatement,
-            'sample_output': outputStatement,
-            'problems': problemStatement
-        }
+        items['sample_input'] = inputStatement,
+        items['sample_output'] = outputStatement,
+        items['problem'] = problemStatement
+        items['problem_name'] = problem_name
+
+        yield items
 
     def parseLeaderBoard(self, response, problem_name):
+        items = Leader()
         datas = response.css('a[data-action="hacker-modal"]')
         for data in datas:
             username = data.css('::attr(username)').extract_first()
             rank = data.css('::attr(data-attr8)').extract_first()
             score = data.css('::attr(data-attr10)').extract_first()
-            yield {
-                'username': username,
-                'rank': rank,
-                'score': score,
-                'data_name': problem_name
-            }
+            items['username'] = username,
+            items['rank'] = rank,
+            items['score'] = score,
+            yield items
